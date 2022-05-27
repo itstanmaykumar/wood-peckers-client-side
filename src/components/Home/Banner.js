ae7a09f170b71d30abc3b9c7c7db1cbac0a5f5aa
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-27 23:46:31 +0600

    added hooks for fetching products & single product

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
                 <div className="col-lg-6 order-lg-1 pe-4">
                     <p className="mb-2 text-second ">15% OFF EVERYTHING</p>
                     <h1 className="mb-3">Buy The <span className="text-main">Finest Sationary Products</span> of Your Office Or Eductaional Institutions</h1>
-                    <small className="mb-2 d-block text-secondary">Find top-selling posters and art prints and make them yours with quality canvas, handcrafted frames, or seek wood mounts.</small>
+                    <small className="mb-2 d-block text-secondary">Find top-selling stationary products and buy them in a wholesell price. We are just one click away.</small>
                     <Link className="mt-4 px-4 border-2 fw-bolder btn btn-outline-main" to="/products">Explore <i className="ps-1 fas fa-angle-double-right"></i></Link>
                 </div>
             </div>
