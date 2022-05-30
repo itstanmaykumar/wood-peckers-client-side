a2c865c9f4d762c78bd20fbe67cb31965fc0d6f2
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-30 07:25:00 +0600

    fixed newsletter

--- src/components/Home/Newsletter.js
+++ src/components/Home/Newsletter.js
@@ -8,7 +8,17 @@ const Newsletter = () => {
 
     const handleNewsletter = (e) => {
         e.preventDefault();
-        toast("Newsletter Subscription Successful!")
+        e.target.email.value = "";
+        e.target.alert.checked = false;
+        e.target.bttn.disabled = true;
+        const resolveAfter1Sec = new Promise(resolve => setTimeout(resolve, 1500));
+        toast.promise(
+            resolveAfter1Sec,
+            {
+                pending: 'Processing... Please wait.',
+                success: 'Newsletter Subscription Successful!',
+            }
+        );
     }
 
     const [user, setUser] = useState({});
@@ -32,15 +42,15 @@ const Newsletter = () => {
                 <div className="col-lg-6 ps-5">
                     <p className="mb-2 text-second ">GET DISCOUNT ALERTS REGULARLY</p>
                     <h3 className="mb-4">Subcribe To <span className="text-main">Our Newsletter</span></h3>
-                    <form className='col-10'>
-                        <input type="email" className="form-control" placeholder="Your Email Address" defaultValue={user.email} aria-label="Email Address" required />
+                    <form onSubmit={handleNewsletter} className='col-10'>
+                        <input type="email" name="email" className="form-control" placeholder="Your Email Address" defaultValue={user.email} aria-label="Email Address" disabled={user.email} required />
                         <div className="form-check my-2">
-                            <input className="form-check-input bg-dark" type="checkbox" id="gridCheck" defaultChecked />
+                            <input className="form-check-input bg-dark" type="checkbox" id="gridCheck" defaultChecked name="alert" required />
                             <label className="form-check-label" htmlFor="gridCheck">
                                 Get Offers & Discount Alerts
                             </label>
                         </div>
-                        <button className='mt-3 btn btn-main' type='submit'>Subscribe Now</button>
+                        <button className='mt-3 btn btn-main' type='submit' name="bttn" >Subscribe Now</button>
                     </form>
                 </div>
             </div>
